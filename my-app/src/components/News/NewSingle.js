7460b4cc5974e49173915dd235b737eb591e1d13
christian baidoo <chris_pens@hotmail.co.uk>
2020-05-13 21:58:13 +0100

    fixed a security risk with the links

--- my-app/src/components/News/NewSingle.js
+++ my-app/src/components/News/NewSingle.js
@@ -2,21 +2,26 @@ import React from "react";
 
 const NewsSingle = ({ item }) => (
   <div className="col s4">
+
     <div className="card">
+      
       <div className="card-img-top">
         <img src={item.urlToImage} alt={item.title} />
         <span className="card-title">{item.source.name}</span>
-
       </div>
+
       <div className="card-content">
         <p>{item.title}</p>
       </div>
+
       <div className="card-action">
-        <a href={item.url} target="_blank">
+        <a href={item.url} target="_blank"  rel="noopener noreferrer">
           Full article
         </a>
       </div>
+
     </div>
+
   </div>
 );
 

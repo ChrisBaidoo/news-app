2613809eac73cd6c383c1610aad37f37e876a3e4
christian baidoo <chris_pens@hotmail.co.uk>
2020-06-05 23:23:54 +0100

    changed the colour of the navbar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -4,13 +4,13 @@ export default function NavBar() {
     return (
         <div> 
           
-         <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark ">
+   <nav className="navbar navbar-expand-lg navbar navbar-light bg-light ">
 
          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
            <span className="navbar-toggler-icon"></span>
          </button>
 
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
       <a className="nav-item nav-link" href="bbc">BBC</a>
       <a className="nav-item nav-link" href="technology">Technology</a>
